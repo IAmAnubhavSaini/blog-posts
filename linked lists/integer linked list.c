9e96441a0a63df290c6a6aeb2ad7eb8413a496e4
Anubhav Saini <Anubhav_Saini-TW@external.mckinsey.com>
2014-11-30 15:13:13 +0530

    DS - Integer Linked List - Implementing linked list for integers.

---
+++ linked lists/integer linked list.c
@@ -0,0 +1,121 @@
+#include "integer linked list.h"
+
+Node *head = NULL;
+Node *curr = NULL;
+
+Node* create_list(int val){
+	Node *ptr = (Node*)malloc(sizeof(Node));
+	if (ptr == NULL){
+		printf("\n*Cannot create a node.*\n");
+		return NULL;
+	}
+
+	ptr->val = val;
+	ptr->next = NULL;
+
+	head = curr = ptr;
+	return ptr;
+}
+
+Node* add_to_list(int val, bool before_head){
+	Node *newTS = (Node*)malloc(sizeof(Node));
+	if (head == NULL){
+		return(create_list(val));
+	}
+
+	if (before_head) {
+		newTS->val = val;
+		newTS->next = head;
+		head = newTS;
+		return head;//try returning head so that further manipulation can start from right there.
+	}
+	else{
+		newTS->val = val;
+		newTS->next = NULL;
+		curr->next = newTS;
+		curr = newTS;
+		return curr;//try returning current so that further manipulation can start from right there.
+	}
+}
+
+void print_list(){
+
+	Node *ptr = head;
+	if (ptr != NULL){
+		printf("\nThe values of the list are\n");
+		while (ptr){
+			printf("[%d @ %p]->", ptr->val, ptr);
+			ptr = ptr->next;
+		}
+		printf("[NULL]\n");
+	}
+	else{
+		printf("\nEmpty list.");
+	}
+}
+
+bool search_list(int val){
+	Node * start = head;
+	bool isFound = false;
+	while (start != NULL){
+		if (start->val == val){
+			isFound = true;
+			break;
+		}
+		start = start->next;
+	}
+	return isFound;
+}
+
+bool delete_first_value_matching_node(int val){
+	Node * prev = head;
+	Node * curr = NULL;
+	bool isDeleted = false;
+	if (head->next != NULL){
+		curr = head->next;
+		printf("\ndebug: at least two nodes exists.");
+		while (curr != NULL && !isDeleted){
+			if (curr->val == val){
+				prev->next = curr->next;
+				isDeleted = true;
+				//free curr here
+			}
+			prev = curr;
+			curr = curr->next;
+		}
+	}
+	else{
+		printf("\ndebug: only one node exists.");
+		if (prev->val == val){
+			isDeleted = true;
+			//free prev
+			prev = NULL;
+		}
+	}
+	return isDeleted;
+}
+
+bool delete_all_value_matching_nodes(int val){
+	Node * prev = head;
+	Node * curr = NULL;
+	bool isDeleted = false;
+	if (head->next != NULL){
+		curr = head->next;
+		while (curr != NULL){
+			if (curr->val == val){
+				isDeleted = true;//at least deleted once.
+				prev->next = curr->next;
+				//free curr
+			}
+			prev = curr;
+			curr = curr->next;
+		}
+	}
+	else{//there is only one node
+		if (prev->val == val){
+			isDeleted = true;
+			prev = NULL;
+		}
+	}
+	return isDeleted;
+}
---
+++ linked lists/integer linked list.h
@@ -0,0 +1,22 @@
+#ifndef INTEGER_LINKED_LIST_H
+#define INTEGER_LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct link_node
+{
+	int val;
+	struct link_node *next;
+};
+typedef struct link_node Node; //gives a bit of OOP feel
+
+Node  *   create_list(int val);
+Node  *   add_to_list(int val, bool before_head);
+void      print_list();
+bool      search_list(int val);
+bool      delete_first_value_matching_node(int val);
+bool      delete_all_value_matching_nodes(int val);
+
+#endif INTEGER_LINKED_LIST_H

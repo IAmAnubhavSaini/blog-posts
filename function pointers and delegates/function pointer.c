0c8d8ddcecc066e103077b22e44d4a60bb85b320
Anubhav Saini <Anubhav_Saini-TW@external.mckinsey.com>
2014-11-30 13:23:25 +0530

    Milestone#2 FPD - function pointer in C
    function_pointer.c
    function_pointer_dump.c
    function_pointer_executions.txt

---
+++ function pointers and delegates/function pointer.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void ActionCarrier(int(*action)(int, int), int a, int b);
+
+int sum(int a, int b);
+int difference(int a, int b);
+int product(int a, int b);
+int quotient(int a, int b);
+int modulus(int a, int b);
+
+int main(){
+	int a = 20, b = 7;
+	ActionCarrier(sum, a, b);
+	ActionCarrier(difference, a, b);
+	ActionCarrier(product, a, b);
+	ActionCarrier(quotient, a, b);
+	ActionCarrier(modulus, a, b);
+}
+
+void ActionCarrier(int(*action)(int, int), int a, int b){
+	printf("\n%d %ld %d = %d", a, action, b, action(a, b));
+}
+
+int sum(int a, int b){
+	return a + b;
+}
+int difference(int a, int b){
+	return a - b;
+}
+int product(int a, int b){
+	return a*b;
+}
+int quotient(int a, int b){
+	return a / b;
+}
+int modulus(int a, int b){
+	return a%b;
+}

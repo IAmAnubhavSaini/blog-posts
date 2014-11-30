3de80060d8c51f1f8d4129f2208764d5a89329db
Anubhav Saini <Anubhav_Saini-TW@external.mckinsey.com>
2014-11-30 17:37:59 +0530

    DS - Interger Linked List - Improving runner code.

--- linked lists/integer linked list runner.c
+++ linked lists/integer linked list runner.c
@@ -1,10 +1,23 @@
 #include "integer linked list test.h"
 
-int main(){
+void fire(){
+	printf("\n\nFiring test 1\n");
 	TEST_setup1();
+	printf("\n\nFiring test 2\n");
 	TEST_search1();
+	printf("\n\nFiring test 3\n");
 	TEST_delete1();
+	printf("\n\nFiring test 4\n");
 	TEST_setup1();
+	printf("\n\nFiring test 5\n");
 	TEST_delete2();
+	printf("\n\nFiring test 6\n");
+	TEST_circular_list();
+
+}
+
+int main(){
+	fire();
 	return 0;
 }
+

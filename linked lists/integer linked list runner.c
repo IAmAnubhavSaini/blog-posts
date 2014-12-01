1d8fff50ea8770262aba0c2072d00e805bfe5bb7
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-01 15:20:07 +0530

    Cleanup - FT_n converted to FTN

--- linked lists/integer linked list runner.c
+++ linked lists/integer linked list runner.c
@@ -1,20 +1,22 @@
 #include "integer linked list test.h"
 
 void fireTests(){
-	printf(FT_1);
+	printf(FTN, 1);
 	TEST_should_setup_and_confim_via_printing();
-	printf(FT_2);
+	printf(FTN, 2);
 	TEST_should_find_number_in_list();
-	printf(FT_3);
+	printf(FTN, 3);
 	TEST_should_delete_numbers_first_occurence();
-	printf(FT_4);
+	printf(FTN, 4);
 	TEST_should_delete_all_occurences_on_a_number();
-	printf(FT_5);
+	printf(FTN, 5);
 	TEST_shoud_not_be_a_circular_list();
-	printf(FT_6);
+	printf(FTN, 6);
 	TEST_shoud_be_a_circular_list();
-	printf(FT_7);
+	printf(FTN, 7);
 	TEST_should_not_find_number_in_list();
+	printf(FTN, 8);
+	TEST_stack_should_be_reverse_of_the_list();
 }
 
 int main(){
--- linked lists/testing constants.h
+++ linked lists/testing constants.h
@@ -1,13 +1,7 @@
 #ifndef TESTING_CONSTANTS_H
 #define TESTING_CONSTANTS_H
 
-#define FT_1 "\n\nFiring test 1\n"
-#define FT_2 "\n\nFiring test 2\n"
-#define FT_3 "\n\nFiring test 3\n"
-#define FT_4 "\n\nFiring test 4\n"
-#define FT_5 "\n\nFiring test 5\n"
-#define FT_6 "\n\nFiring test 6\n"
-#define FT_7 "\n\nFiring test 7\n"
+#define FTN "\n\nFiring test %d\n"
 
 #define FNIL "\nFound %d in list."
 #define DNFNIL "\nDid not find %d in list."

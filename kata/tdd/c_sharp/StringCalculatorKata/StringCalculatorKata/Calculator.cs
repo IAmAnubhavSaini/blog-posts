2183d03a93b60602a1be3f7e3fd270505accc983
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-04 20:33:57 +0530

    KATA - string calculator - do not allow more than 3 numbers.
    That's the first problem.
    
    http://osherove.com/tdd-kata-1/

--- kata/tdd/c_sharp/StringCalculatorKata/StringCalculatorKata/Calculator.cs
+++ kata/tdd/c_sharp/StringCalculatorKata/StringCalculatorKata/Calculator.cs
@@ -6,6 +6,8 @@ namespace StringCalculatorKata
     {
         public static int Add(string numbers)
         {
+            if (numbers.Split(',').Length > 3)
+                return 0;
             return string.IsNullOrEmpty(numbers) ? 0 : numbers.Split(',').Sum(num => int.Parse(num));
         }
     }

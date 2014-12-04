8f2116f12e328f033f56e1ab781679d5ef53eeae
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-04 20:18:15 +0530

    KATA - string calculator in C#

---
+++ kata/tdd/c_sharp/StringCalculatorKata/StringCalculatorKata.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturnZeroWhenInputNumberStringIsEmpty()
+        {
+            var numbers = string.Empty;
+
+            Assert.IsTrue(0 == Calculator.Add(numbers));
+        }
+
+
+    }
+}
---
+++ kata/tdd/c_sharp/StringCalculatorKata/StringCalculatorKata/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringCalculatorKata
+{
+    public class Calculator
+    {
+        public static int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}

4ffb48e84084190661ca6e51648cfc718bf812e3
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-06 08:18:55 +0530

    DS - STACK - integer stack - more tests.

--- stack/stack test runner.c
+++ stack/stack test runner.c
@@ -1,6 +1,11 @@
 #include "stack test.h"
 
 int main(){
+	// tests that will not stay forever, since aborting will be replaced with better code.
 	// TEST_should_abort_when_empty_stack_popped(); // tested. ok.
+	// TEST_should_abort_on_push_to_full_stack();
+
+	// tests that are there to stay.
+	// TEST_should_push_and_pop_three_integers_to_stack(); // tested,
 	return 0;
 }
--- stack/stack test.c
+++ stack/stack test.c
@@ -5,3 +5,25 @@ void TEST_should_abort_when_empty_stack_popped()
 	struct stack_head * head = init_stack(1);
 	pop(head);
 }
+
+void TEST_should_abort_on_push_to_full_stack()
+{
+	struct stack_head * stack = init_stack(3);
+	printf("\nRunning: TEST_should_abort_on_push_to_full_stack.");
+	push(stack, 1);
+	push(stack, 2);
+	push(stack, 3);
+	push(stack, 4); // should be aborted here.
+}
+
+void TEST_should_push_and_pop_three_integers_to_stack()
+{
+	struct stack_head * stack = init_stack(3);
+	printf("\nRunning: TEST_should_push_and_pop_three_integers_to_stack.");
+	push(stack, 1);
+	push(stack, 2);
+	push(stack, 3);
+	printf("\nStack top: %d,", pop(stack));
+	printf("\nStack top: %d,", pop(stack));
+	printf("\nStack top: %d,", pop(stack));
+}

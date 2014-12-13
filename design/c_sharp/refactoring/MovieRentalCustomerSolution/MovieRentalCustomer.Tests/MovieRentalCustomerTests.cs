7b0b732989679d10ec57aec3caab168feb2bad32
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-13 13:29:07 +0530

    DESIGN - REFACTORING - MovieRentalCustomer - First and basic test.

---
+++ design/c_sharp/refactoring/MovieRentalCustomerSolution/MovieRentalCustomer.Tests/MovieRentalCustomerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieRentalCustomer.Tests
+{
+    [TestClass]
+    public class MovieRentalCustomerTests
+    {
+        [TestMethod]
+        public void OutputStringTestShouldPass()
+        {
+            Customer newCustomer = new Customer("Anubhav");
+            newCustomer.AddRental(new Rental(new Movie("Termitators", MovieType.NewRelease), 2));
+
+            Assert.AreEqual(newCustomer.GenerateStatement(), "Rental reocord for Anubhav.\n\tTermitators\t6\nAmount owed is 6\nYou earned 2 frequent renter points");
+        }
+    }
+}

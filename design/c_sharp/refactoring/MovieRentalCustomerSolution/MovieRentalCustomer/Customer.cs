24bd02dd4eee8e01cdb25ea03317606751adf6f9
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-13 15:10:14 +0530

    DESIGN - REFACTORING - MovieRentalCustomer - Refactoring functions out of huge method GenerateStatement.

--- design/c_sharp/refactoring/MovieRentalCustomerSolution/MovieRentalCustomer/Customer.cs
+++ design/c_sharp/refactoring/MovieRentalCustomerSolution/MovieRentalCustomer/Customer.cs
@@ -27,37 +27,43 @@ namespace MovieRentalCustomer
             var result = "Rental reocord for " + Name + ".\n";
             foreach (var rental in Rentals)
             {
-                var thisAmount = 0d;
-                switch (rental.Movie.Type)
-                {
-                    case MovieType.Children:
-                        thisAmount += 1.5;
-                        if (rental.DaysRented > 3)
-                            thisAmount += (rental.DaysRented - 3)*1.5;
-                        break;
-                    case MovieType.NewRelease:
-                        thisAmount += rental.DaysRented*3;
-                        break;
-                    case MovieType.Regular:
-                        thisAmount += 2;
-                        if (rental.DaysRented > 2)
-                            thisAmount += (rental.DaysRented - 2)*1.5;
-                        break;
-                }
-
-                frequentRenterPoints++;
-
-                if ((rental.Movie.Type == MovieType.NewRelease) && rental.DaysRented > 1)
-                    frequentRenterPoints++;
-
-
-                result += "\t" + rental.Movie.Title + "\t" + thisAmount + "\n";
-
-                totalAmount += thisAmount;
+                frequentRenterPoints += FrequentRenterPoints(rental);
+                result += "\t" + rental.Movie.Title + "\t" + RentalAmount(rental) + "\n";
+                totalAmount += RentalAmount(rental);
             }
             result += "Amount owed is " + totalAmount + "\n";
             result += "You earned " + frequentRenterPoints + " frequent renter points";
             return result;
+        }
+
+        private static int FrequentRenterPoints(Rental rental)
+        {
+            var frequentRenterPoints = 1;
+            if ((rental.Movie.Type == MovieType.NewRelease) && rental.DaysRented > 1)
+                frequentRenterPoints++;
+            return frequentRenterPoints;
+        }
+
+        private static double RentalAmount(Rental rental)
+        {
+            var thisAmount = 0d;
+            switch (rental.Movie.Type)
+            {
+                case MovieType.Children:
+                    thisAmount += 1.5;
+                    if (rental.DaysRented > 3)
+                        thisAmount += (rental.DaysRented - 3) * 1.5;
+                    break;
+                case MovieType.NewRelease:
+                    thisAmount += rental.DaysRented * 3;
+                    break;
+                case MovieType.Regular:
+                    thisAmount += 2;
+                    if (rental.DaysRented > 2)
+                        thisAmount += (rental.DaysRented - 2) * 1.5;
+                    break;
+            }
+            return thisAmount;
         }
     }
 }

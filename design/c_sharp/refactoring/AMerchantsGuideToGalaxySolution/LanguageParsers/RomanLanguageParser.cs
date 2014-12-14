af2bcdaf753720658456bd69a6336a89fbb02418
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-14 13:28:19 +0530

    DESIGN - REFACTORING - AMerchantsGuideToGalaxy - A crude roman language parser is implemented and with tests.

---
+++ design/c_sharp/refactoring/AMerchantsGuideToGalaxySolution/LanguageParsers/RomanLanguageParser.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace LanguageParsers
+{
+    public class RomanLanguageParser
+    {
+        private static Dictionary<string, int> RomanToDecimalDictionary { get; set; }
+
+        static RomanLanguageParser()
+        {
+            RomanToDecimalDictionary = new Dictionary<string, int>
+            {
+                {"I", 1},
+                {"V", 5},
+                {"X", 10},
+                {"L", 50},
+                {"C", 100},
+                {"D", 500},
+                {"M", 1000}
+            };
+        }
+        public static int GetValueOfRomanNumber(string number)
+        {
+            if (string.IsNullOrEmpty(number))
+                throw new ArgumentNullException();
+
+            if (RomanToDecimalDictionary.ContainsKey(number))
+            {
+                return RomanToDecimalDictionary[number];
+            }
+            if (number.Length > 1)
+            {
+                return Parse(number);
+            }
+            throw new Exception("Value not found for " + number);
+        }
+
+        private static int Parse(string number)
+        {
+            var totalValue = 0;
+
+            for (var i = 0; i < number.Length; ++i)
+            {
+                var curr = number[i].ToString(CultureInfo.InvariantCulture);
+                if (NextExists(i, number))
+                {
+                    if (NextIsBigger(i, number))
+                    {
+                        totalValue += GetValueOfRomanNumber(Next(i, number)) - GetValueOfRomanNumber(curr);
+                        i++;
+                    }
+                    else
+                    {
+                        totalValue += GetValueOfRomanNumber(curr);
+                    }
+                }
+                else
+                {
+                    totalValue += GetValueOfRomanNumber(curr);
+                }
+            }
+            return totalValue;
+        }
+
+        private static bool NextExists(int currentIndex, string number)
+        {
+            return number.Length > currentIndex + 1;
+        }
+
+        private static string Next(int currentIndex, string number)
+        {
+            return number[currentIndex + 1].ToString(CultureInfo.InvariantCulture);
+        }
+
+        private static bool NextIsBigger(int currentIndex, string number)
+        {
+            return currentIndex + 1 < number.Length &&
+                   GetValueOfRomanNumber(number[currentIndex].ToString(CultureInfo.InvariantCulture))
+                   <
+                   GetValueOfRomanNumber(number[currentIndex + 1].ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ design/c_sharp/refactoring/AMerchantsGuideToGalaxySolution/Tests/LanguageParsers/RomanLanguageParserTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using LanguageParsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.LanguageParsers
+{
+    [TestClass]
+    public class RomanLanguageParserTests
+    {
+        [TestMethod]
+        public void BasicRomanNumeralConversionsShouldPass()
+        {
+            Assert.AreEqual(1, RomanLanguageParser.GetValueOfRomanNumber("I"));
+            Assert.AreEqual(5, RomanLanguageParser.GetValueOfRomanNumber("V"));
+            Assert.AreEqual(10, RomanLanguageParser.GetValueOfRomanNumber("X"));
+            Assert.AreEqual(50, RomanLanguageParser.GetValueOfRomanNumber("L"));
+            Assert.AreEqual(100, RomanLanguageParser.GetValueOfRomanNumber("C"));
+            Assert.AreEqual(500, RomanLanguageParser.GetValueOfRomanNumber("D"));
+            Assert.AreEqual(1000, RomanLanguageParser.GetValueOfRomanNumber("M"));
+        }
+
+        [TestMethod]
+        public void AdvancedRomanNumeralConversionShouldPass()
+        {
+            Assert.AreEqual(2, RomanLanguageParser.GetValueOfRomanNumber("II"));
+            Assert.AreEqual(3, RomanLanguageParser.GetValueOfRomanNumber("III"));
+            Assert.AreEqual(4, RomanLanguageParser.GetValueOfRomanNumber("IV"));
+            Assert.AreEqual(6, RomanLanguageParser.GetValueOfRomanNumber("VI"));
+            Assert.AreEqual(9, RomanLanguageParser.GetValueOfRomanNumber("IX"));
+            Assert.AreEqual(11, RomanLanguageParser.GetValueOfRomanNumber("XI"));
+            Assert.AreEqual(2014, RomanLanguageParser.GetValueOfRomanNumber("MMXIV"));
+        }
+
+        [TestMethod]
+        public void ShouldFailWhenRomanNumberStringIsEmpty()
+        {
+            Assert.AreEqual(new ArgumentNullException(),RomanLanguageParser.GetValueOfRomanNumber(string.Empty));
+        }
+    }
+}

83c6808455e4de8d04ac98794574340474f3d790
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-25 01:21:33 +0530

    DESIGN - REFACTORING - Fizz Buzz FizzBuzz - Sayer added. This will help in strategy pattern.

---
+++ design/c_sharp/refactoring/FizzBuzzSolution/FizzBuzz/Sayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzFizzBuzz
+{
+    public class Sayer : ISaySomething
+    {
+        protected string WhatToSay { get; set; }
+        protected int Divisor { get; set; }
+        protected int Number { get; set; }
+
+        public virtual string Say()
+        {
+            return Number.ToString();
+        }
+
+        public bool ShouldSay()
+        {
+            return Number%Divisor == 0;
+        }
+    }
+}

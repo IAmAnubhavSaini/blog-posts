c69f26ec192f5f311c4f50f13828184ef18de7d0
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-25 00:43:48 +0530

    DESIGN - REFACTORING - AMerchantsGuideToGalaxy2 - Add GuideTest project and reference to Exceptions project in .sln file.

---
+++ design/c_sharp/refactoring/AMerchantsGuideToGalaxySolution2/GuideTests/QuestionTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuideTests
+{
+    [TestClass]
+    public class QuestionTests
+    {
+        [TestMethod]
+        public void ShouldPassIfQuestionStringContainsHow()
+        {
+
+        }
+    }
+}

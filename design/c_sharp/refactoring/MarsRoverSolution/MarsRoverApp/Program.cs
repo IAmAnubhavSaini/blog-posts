09261dac1c77ee1df962a34c52afc08ea1ee782e
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-12-25 10:20:29 +0530

    DESIGN - DEVELOPMENT - Mars Rover - Crude implementation of the mars rover app. Tests are fine.

--- design/c_sharp/refactoring/MarsRoverSolution/MarsRoverApp/Program.cs
+++ design/c_sharp/refactoring/MarsRoverSolution/MarsRoverApp/Program.cs
@@ -1,10 +1,59 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MarsRover;
+
 namespace MarsRoverApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var rovers = new List<Rover>();
+            var input = Console.ReadLine();
+            if(string.IsNullOrEmpty(input))
+                throw new Exception("Input not recieved.");
+            var planeTopRightCoordinates = input.Split(' ');
+            if (planeTopRightCoordinates.Count() != 2)
+            {
+                throw new Exception("Bad input");
+            }
+            var plane = new Plane(int.Parse(planeTopRightCoordinates[0]), int.Parse(planeTopRightCoordinates[1]));
+
+            while (!string.IsNullOrEmpty(input = Console.ReadLine()))
+            {
+                var locationStrings = input.Split(' ');
+                if(locationStrings.Count()!=3)
+                    throw new Exception("Bad input");
+                var direction = CompassDirection.East;
+                switch (locationStrings[2].ToUpper())
+                {
+                    case "N": direction = CompassDirection.North; 
+                        break;
+                    case "E": direction = CompassDirection.East;
+                        break;
+                    case "W" : direction = CompassDirection.West;
+                        break;
+                    case "S" : direction=CompassDirection.South;
+                        break;
+                    default: throw new Exception("Bad direction input.");
+                }
+                var location = new Location(int.Parse(locationStrings[0]),
+                    int.Parse(locationStrings[1]), direction);
+
+                var command = Console.ReadLine();
+                if(string.IsNullOrEmpty(command)) throw new Exception("Command not received.");
+
+                var rover = new Rover(plane, command, location);
+                rovers.Add(rover);
+            }
+
+            foreach (var rover in rovers)
+            {
+                var finalLocation = rover.Operate();
+                Console.WriteLine(finalLocation.CurrentX + " " + finalLocation.CurrentY + " " + finalLocation.CurrentDirection.ToString()[0]);
+            }
         }
     }
 }

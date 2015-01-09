350dd5755f7b9825d55554aa1db6b6cc5d49566a
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-01-09 14:01:30 +0530

    C_SHARP - PUZZLE SOLVER - DEVELOPMENT - Basic implementation started.

---
+++ c_sharp/PuzzleSolverSolution/EnglishDictionaryLibrary/Words.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EnglishDictionaryLibrary
+{
+    public class Words
+    {
+        private const string OxfordEnglishDictionary = "oxford_english_dictionary.txt";
+
+        public IEnumerable<string> GetWords(string folderPath, string fileName = OxfordEnglishDictionary)
+        {
+            var words = new List<string>();
+            if (Directory.Exists(folderPath))
+            {
+                if (File.Exists(folderPath + fileName))
+                {
+                    
+                }
+            }
+            return words;
+        }
+    }
+}

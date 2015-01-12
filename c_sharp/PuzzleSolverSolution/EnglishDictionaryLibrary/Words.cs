d66d360d88eef02d68f2afc5d5ed7a7f203506e3
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-01-12 10:38:42 +0530

    C_SHARP - PUZZLE SOLVER - DEVELOPMENT - Cleanup.

--- c_sharp/PuzzleSolverSolution/EnglishDictionaryLibrary/Words.cs
+++ c_sharp/PuzzleSolverSolution/EnglishDictionaryLibrary/Words.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
 namespace EnglishDictionaryLibrary
 {
@@ -17,7 +12,7 @@ namespace EnglishDictionaryLibrary
 
         public IEnumerable<string> AllWords { get; set; }
 
-        public IEnumerable<string> GetWordsThatContain(IEnumerable<char> letters, int minLenght, int maxLength)
+        public IEnumerable<string> GetWordsThatContainAllLetters(IEnumerable<char> letters, int minLenght, int maxLength)
         {
             var words = AllWords.Where(w => w.Length >= minLenght && w.Length <= maxLength);
             return letters.Aggregate(words, (current, l) => current.Where(w => w.Contains(l)));
--- c_sharp/PuzzleSolverSolution/PuzzleSolverConsole/Program.cs
+++ c_sharp/PuzzleSolverSolution/PuzzleSolverConsole/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using System.Globalization;
+using EnglishDictionaryLibrary;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using EnglishDictionaryLibrary;
 
 namespace PuzzleSolverConsole
 {
@@ -11,13 +12,13 @@ namespace PuzzleSolverConsole
         static void Main(string[] args)
         {
             UsageDetails();
-            var pathToOxfordEnglishDictionary = args.Length != 0 ? args[0] : Console.ReadLine();
-            if (!File.Exists(pathToOxfordEnglishDictionary))
+            var pathToWordListFile = args.Length != 0 ? args[0] : Console.ReadLine();
+            if (!File.Exists(pathToWordListFile))
             {
-                Console.WriteLine("File not found at : {0}", pathToOxfordEnglishDictionary);
+                Console.WriteLine("File not found at : {0}", pathToWordListFile);
                 return;
             }
-            var input = ReadLinesFromOEd(pathToOxfordEnglishDictionary);
+            var input = ReadAllLinesFromFile(pathToWordListFile);
             var wordsList = GetWords(input);
             var wordType = new Words(wordsList);
 
@@ -31,10 +32,10 @@ namespace PuzzleSolverConsole
                 Console.WriteLine("Letters not provided.");
                 return;
             }
-            var minLenghtOfDesiredWords = int.Parse(args.Length > 0 && args[2] != null ? args[2] : Console.ReadLine());
-            var maxLenghtOfDesiredWords = int.Parse(args.Length > 0 && args[3] != null ? args[3] : Console.ReadLine());
+            var minLenghtOfDesiredWords = int.Parse(args.Length > 0 && args[2] != null ? args[2] : (Console.ReadLine() ?? "4"));
+            var maxLenghtOfDesiredWords = int.Parse(args.Length > 0 && args[3] != null ? args[3] : (Console.ReadLine() ?? "4"));
 
-            foreach (var word in wordType.GetWordsThatContain(letters, minLenghtOfDesiredWords, maxLenghtOfDesiredWords))
+            foreach (var word in wordType.GetWordsThatContainAllLetters(letters, minLenghtOfDesiredWords, maxLenghtOfDesiredWords))
             {
                 Console.WriteLine(word);
             }
@@ -43,7 +44,7 @@ namespace PuzzleSolverConsole
         private static void UsageDetails()
         {
             Console.WriteLine("If you haven't invoked the program using following style then you need to enter details.");
-            Console.WriteLine("<program> <oxford_english_dict.txt> <minLength> <maxLength>");
+            Console.WriteLine("<program> <word list full path> <min word len> <max word len>");
         }
 
         private static IEnumerable<string> GetWords(string input)
@@ -52,12 +53,12 @@ namespace PuzzleSolverConsole
             return allLines.Select(line => line.Split(' ')[0]).ToList();
         }
 
-        private static string ReadLinesFromOEd(string pathToOxfordEnglishDictionary)
+        private static string ReadAllLinesFromFile(string pathToWordListFile)
         {
-            var oedReader = new StreamReader(pathToOxfordEnglishDictionary);
-            var input = oedReader.ReadToEnd();
-            oedReader.Close();
-            oedReader.Dispose();
+            var reader = new StreamReader(pathToWordListFile);
+            var input = reader.ReadToEnd().ToLower(CultureInfo.InvariantCulture);
+            reader.Close();
+            reader.Dispose();
             return input;
         }
     }

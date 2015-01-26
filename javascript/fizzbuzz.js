2da044348219c35d2da24782bd931222102ac72b
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-01-25 23:41:45 -0800

    Problem Solving - JavaScript - fizzbuzz - Made the functions private since we don't really need public interface for this problem at all

--- javascript/fizzbuzz.js
+++ javascript/fizzbuzz.js
@@ -1,18 +1,20 @@
-function output(message) {
-    console.log(message);
-}
-
-function FizzBuzz() {
-    for (var i = 1; i < 20; i++) {
-        if (i % 15 === 0)
-            output("fizzbuzz")
-        else if (i % 5 === 0)
-            output("buzz")
-        else if (i % 3 === 0)
-            output("fizz")
-        else
-            output(i);
+(function () {
+    function output(message) {
+        console.log(message);
     }
-}
 
-FizzBuzz();
+    function FizzBuzz() {
+        for (var i = 1; i < 20; i++) {
+            if (i % 15 === 0)
+                output("fizzbuzz")
+            else if (i % 5 === 0)
+                output("buzz")
+            else if (i % 3 === 0)
+                output("fizz")
+            else
+                output(i);
+        }
+    }
+
+    FizzBuzz();
+})();

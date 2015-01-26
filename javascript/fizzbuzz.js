d5c9a8e00e4ee511423ed4d486db5de08e0cea45
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-01-25 23:38:29 -0800

    Problem Solving - JavaScript - fizzbuzz - Crude, ugly but correct implementation

---
+++ javascript/fizzbuzz.js
@@ -0,0 +1,18 @@
+function output(message) {
+    console.log(message);
+}
+
+function FizzBuzz() {
+    for (var i = 1; i < 20; i++) {
+        if (i % 15 === 0)
+            output("fizzbuzz")
+        else if (i % 5 === 0)
+            output("buzz")
+        else if (i % 3 === 0)
+            output("fizz")
+        else
+            output(i);
+    }
+}
+
+FizzBuzz();

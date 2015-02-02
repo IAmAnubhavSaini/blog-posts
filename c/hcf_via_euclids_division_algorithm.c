bcf6c3b2995a81ab68c58e9f67978ebf1c885f7d
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-02-02 15:38:58 -0800

    C - Concept - Via iteration - HCF via Euclid Division Algorithm - Title: "HCF of two numbers via Euclids Division Algorithm in C"
    Read the blog post titled : HCF of two numbers via Euclids Division Algorithm in C.
    Link : http://the-diary-of-a-wimpy-coder.blogspot.com/2015/02/hcf-of-two-numbers-via-euclids-division-algorithm-in-c.html

--- c/hcf_via_euclids_division_algorithm.c
+++ c/hcf_via_euclids_division_algorithm.c
@@ -6,7 +6,8 @@ int main(){
 	int a, b;
 	printf("\nEnter number 1: \n");  scanf("%i", &a); 
 	printf("\nEnter number 2: \n");  scanf("%i", &b); 
-	printf("\nHCF of %i and %i: %i", a, b, hcf_recursive(a, b)); 
+	printf("\nHCF of %i and %i: %i (recursively calculated)", a, b, hcf_recursive(a, b)); 
+	printf("\nHCF of %i and %i: %i (iteratively calculated)", a, b, hcf_iterative(a, b)); 
 	return 0;
 }
 
@@ -24,6 +25,29 @@ int hcf_recursive(int a, int b){
 			return 1;
 		else
 			return hcf_recursive(b, r);
+	}
+	return -1;
+}
+
+int hcf_iterative(int a, int b){
+	int q, r;
+	if(a > 0 && b > 0){
+		if(a < b)
+			return hcf_recursive(b, a);
+
+		q = a / b;
+		r = a % b;
+		while(r != 0 && r != 1){
+			a = b;
+			b = r;
+			q = a / b;
+			r = a % b;
+		}
+		
+		if(r == 0)
+			return b;
+		else if(r == 1)
+			return 1;
 	}
 	return -1;
 }

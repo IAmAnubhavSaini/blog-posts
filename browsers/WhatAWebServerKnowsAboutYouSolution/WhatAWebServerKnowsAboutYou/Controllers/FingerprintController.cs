f4b3498a5913598b4e1dfc6b621387a9881ce42b
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-02-04 23:45:00 -0800

    WEB - What A Web Server Know About You - Refactoring - No need to pass a model as of yet. Just do stuff save and hit F5 on browser. No need to compile.

--- browsers/WhatAWebServerKnowsAboutYouSolution/WhatAWebServerKnowsAboutYou/Controllers/FingerprintController.cs
+++ browsers/WhatAWebServerKnowsAboutYouSolution/WhatAWebServerKnowsAboutYou/Controllers/FingerprintController.cs
@@ -16,29 +16,7 @@ namespace WhatAWebServerKnowsAboutYou.Controllers
 
         public ActionResult Me()
         {
-            List<string> Info = new List<string>();
-            Info.Add("-------------------------------Browser Info-------------------------------");
-            foreach (var key in Request.Browser.Capabilities.Keys)
-            {
-                Info.Add(key + " = " + Request.Browser.Capabilities[key]);
-            }
-            Info.Add("-------------------------------Requst Header Info-------------------------------");
-            for (int i = 0; i < Request.Headers.Count; i++ )
-            {
-                Info.Add(Request.Headers[i]);
-            }
-            Info.Add("-------------------------------Response Header Info-------------------------------");
-            for (int i = 0; i < Response.Headers.Count; i++)
-            {
-                Info.Add(Response.Headers[i]);
-            }
-            Info.Add("-------------------------------Accepted types-------------------------------");
-            foreach (var type in Request.AcceptTypes)
-            {
-                Info.Add(type);
-            }
-            
-            return View(Info);
+            return View();
         }
     }
 }

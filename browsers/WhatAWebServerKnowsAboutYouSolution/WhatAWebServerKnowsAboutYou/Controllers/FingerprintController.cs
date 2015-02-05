13f88ed6afa2c97d0593cdaf5973ad69943be5e7
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-02-04 23:39:13 -0800

    WEB - What A Web Server Know About You - fingerprinting browser - simple and crude implementation.
    Working.

---
+++ browsers/WhatAWebServerKnowsAboutYouSolution/WhatAWebServerKnowsAboutYou/Controllers/FingerprintController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhatAWebServerKnowsAboutYou.Controllers
+{
+    public class FingerprintController : Controller
+    {
+        // GET: Fingerprint
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Me()
+        {
+            List<string> Info = new List<string>();
+            Info.Add("-------------------------------Browser Info-------------------------------");
+            foreach (var key in Request.Browser.Capabilities.Keys)
+            {
+                Info.Add(key + " = " + Request.Browser.Capabilities[key]);
+            }
+            Info.Add("-------------------------------Requst Header Info-------------------------------");
+            for (int i = 0; i < Request.Headers.Count; i++ )
+            {
+                Info.Add(Request.Headers[i]);
+            }
+            Info.Add("-------------------------------Response Header Info-------------------------------");
+            for (int i = 0; i < Response.Headers.Count; i++)
+            {
+                Info.Add(Response.Headers[i]);
+            }
+            Info.Add("-------------------------------Accepted types-------------------------------");
+            foreach (var type in Request.AcceptTypes)
+            {
+                Info.Add(type);
+            }
+            
+            return View(Info);
+        }
+    }
+}

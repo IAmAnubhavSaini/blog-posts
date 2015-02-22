8ed143f84b701057a0e242e395892b9e049aecdc
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-02-23 01:45:25 +0530

    AYOM - 2/365 - Second site created. - MouseTrack

--- web/anubhavsaini.com/365/365.js
+++ web/anubhavsaini.com/365/365.js
@@ -2,7 +2,7 @@ $(function () {
     AYOM = {};
     AYOM.GenerateGreetingSite001 = function () {
         var greeting = $("#Greeting").val();
-        
+
         var fontFamily = $("#FontFamily").val();
         var fontSize = $("#FontSize").val();
         var fontSizeUnit = $("#FontSizeUnit").val();
@@ -11,5 +11,34 @@ $(function () {
         $("#GreetingOutput").text(greeting).css("font-family", fontFamily).css("font-size", fontSize + fontSizeUnit)
         .css("background-color", backgroundColor)
         .css("color", color);
+    };
+
+    AYOM.MouseTrack = function () {
+        var canvas = $("canvas.AYOM.AYOM2#TrackArea")[0],
+            parentOffset = $(canvas).offset(),
+            coordLogArea = $(".AYOM.AYOM2#Log"),
+            coordLog = function (event) {
+
+                return "[" + event.pageX + ", " + event.pageY + "]; relative: [" + (event.pageX - parseInt(parentOffset.left)) + ", " + (event.pageY - parseInt(parentOffset.top)) + "]";
+            },
+            context = canvas.getContext("2d"),
+            pixelSize = 2,
+            color = "#000000",
+            backgroundColor = "#FFffFF";
+
+        $(canvas).on("mousemove", function (event) {
+            $(coordLogArea).text(coordLog(event));
+            context.fillRect((event.pageX - parseInt(parentOffset.left)), (event.pageY - parseInt(parentOffset.top)), pixelSize, pixelSize);
+        });
+
+        $("#Clear").click(function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        });
+
+        $("#Apply").click(function () {
+            pixelSize = $("#Size").val();
+            context.fillStyle = $("#Color").val();
+        });
+
     };
 });
